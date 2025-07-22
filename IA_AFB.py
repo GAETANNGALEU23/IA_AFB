aaf7d640b38efca6296a0833bd7ad50d659f5ef4
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2025-07-22 20:34:19 +0200

    Update IA_AFB.py

--- IA_AFB.py
+++ IA_AFB.py
@@ -38,7 +38,7 @@ def login_page():
                 flex-direction: column;
                 align-items: center;
                 justify-content: start;
-                height: 100vh;
+                height: 30vh;
                 padding-top: 100px;
                 text-align: center;
             }
