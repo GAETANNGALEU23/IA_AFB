cc6dc1610a68003f48ec29b562a8ac5bacdc38d6
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2025-07-22 20:33:07 +0200

    Update IA_AFB.py

--- IA_AFB.py
+++ IA_AFB.py
@@ -119,7 +119,7 @@ def main_page():
                 resize: none;
                 background-color: transparent;
                 font-size: 16px;
-                height: 80px;
+                height: 30px;
                 padding-top: 10px;
             }
             .input-box button {
